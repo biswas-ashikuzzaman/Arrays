0e64a43ce5e3303622c5b35dc3d5ea68fadb3aa1
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-05-29 13:33:17 +0600

    update

---
+++ array_indexing.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana","Orange","Apple","Mango"];
+console.log(fruits);
+console.log(fruits.fill("jam",2,4));
+document.write(fruits.fill("j"));
+console.log(fruits.fill("j"));
---
+++ array_slice.js
@@ -0,0 +1,4 @@
+var nam =[1,2,3,4,5,7,8,3];
+console.log(nam);
+let result =nam.slice(-5,-2);
+console.log(result);
